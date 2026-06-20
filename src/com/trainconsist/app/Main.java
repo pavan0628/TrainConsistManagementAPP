f56fafcb77e3d87e6fad13b8db9c52c8c6050ba8
pavan0628 <pavan.cse.rymec@gmail.com>
2026-06-20 14:42:48 +0530

    [PAVAN] UC2 Passanger bogie arraylist operations

--- src/com/trainconsist/app/Main.java
+++ src/com/trainconsist/app/Main.java
@@ -6,13 +6,36 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) {
-        //welcome message displayed when application start
-        System.out.println("========Train Consist management app========");
 
-        //dynamic collection to store bogies initially empty
-        List<String> trainConsist=new ArrayList<>();
+        //ArrayList to store passanger bogies
+        List<String> passangerBogies=new ArrayList<>();
 
-        //display initial bogie count
-        System.out.println("Initial bogie count: "+trainConsist.size());
+        //add passanger bogies
+        passangerBogies.add("Sleeper");
+        passangerBogies.add("AC chair");
+        passangerBogies.add("First class");
+
+
+        //print list after insertion
+        System.out.println("After addition: "+passangerBogies);
+
+
+        //remove AC Chair
+        passangerBogies.remove("AC chair");
+
+
+        //check whether the sleep exists
+        boolean exists=passangerBogies.contains("Sleeper");
+
+        System.out.println("Sleepr exists: "+exists);
+
+        //final state
+        System.out.println("Final list: "+passangerBogies);
+
+
+
+
+
+
     }
 }
